58855421dde5e60c74f5fdc673a92247c3793770
gigajdn <giga2100016020@webmail.uad.ac.id>
2023-04-12 07:10:46 +0700

    delete

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,24 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-import { AboutComponent } from './homepage/about/about.component';
-import { ContactComponent } from './homepage/contact/contact.component';
-import { ServicesComponent } from './homepage/services/services.component';
-import { FaqComponent } from './homepage/faq/faq.component';
-import { DashboardComponent } from './homepage/dashboard/dashboard.component';
-import { FDataComponent } from './homepage/f-data/f-data.component';
-import { DataComponent } from './homepage/data/data.component';
+
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [ 
   { path: '', redirectTo: '/product', pathMatch: 'full' },
-  { path: 'dashboard',    component: DashboardComponent },
-  { path: 'about',        component: AboutComponent},
-  { path: 'contact',      component: ContactComponent},
-  { path: 'services',     component: ServicesComponent},
-  { path: 'faq',          component: FaqComponent},
-  { path: 'mhs',          component:FDataComponent},
-  { path: 'formulir',     component:DataComponent},
   { path: 'product',      component:ProductComponent}
 ];
 
--- src/app/homepage/dashboard/mahasiswa.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Mahasiswa } from './mahasiswa.model';
-
-describe('MahasiswaModel', () => {
-  it('should create an instance', () => {
-    expect(Mahasiswa).toBeTruthy();
-  });
-});
